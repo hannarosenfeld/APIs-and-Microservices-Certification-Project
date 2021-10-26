2a22e0826f1b15964af8c0ae06b0b9f66501c707
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-26 12:03:48 -0400

    Add invalid date endpoint.

--- server.js
+++ server.js
@@ -22,9 +22,17 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+    console.log({greeting: 'hello API'})
   res.json({greeting: 'hello API'});
 });
 
+// the API endpoint is GET [project_url]/api/timestamp/:data_string?
+app.get("/api/:date_string", function(req, res) {
+    let dateString = req.params.date_string
+    console.log(dateString)
+    
+    res.json({"error" : "Invalid Date"})
+})
 
 
 // listen for requests :)
