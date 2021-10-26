0fa0f75840718a19c1a8d237d86de05100010f71
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-26 14:07:50 -0400

    Add ability to receive stimestamps.

--- server.js
+++ server.js
@@ -16,26 +16,40 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
     console.log({greeting: 'hello API'})
-  res.json({greeting: 'hello API'});
+    res.json({greeting: 'hello API'});
 });
+
+app.get("/api", function(req, res){
+    var now = new Date()
+    res.json({
+        "unix": now.getTime(),
+        "utc": now.toUTCString()
+    })
+})
 
 // the API endpoint is GET [project_url]/api/timestamp/:data_string?
 app.get("/api/:date_string", function(req, res) {
     let dateString = req.params.date_string
-    console.log(dateString)
-    
-    res.json({ "error" : "Invalid Date" })
+    let passedInValue = new Date(dateString)
+
+    passedInValue == "Invalid Date"
+                      ? res.json({ "error" : "Invalid Date" })
+                      : res.json({
+                          "unix": passedInValue.getTime(),
+                          "utc": passedInValue.toUTCString()
+                      })
 })
+
 
 
 // listen for requests :)
 var listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
